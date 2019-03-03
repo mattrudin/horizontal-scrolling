4776e22c33add7b03ac918feb8d87fc32d64d3e9
Matthias Rudin <matthias.rudin@live.com>
2019-03-03 12:11:10 +0100

    Build basic structure

--- src/App.js
+++ src/App.js
@@ -6,21 +6,22 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="slider">
+        <section>
+         <h1>Section One</h1>
+        </section>
+        <section>
+          <h1>Section Two</h1>
+        </section>
+        <section>
+          <h1>Section Three</h1>
+        </section>
+        <section>
+          <h1>Section Four</h1>
+        </section>
+        <section>
+          <h1>Section Five</h1>
+        </section>
       </div>
     );
   }
