1ca72c2258ea44b86faaf772d2707d578be5291c
Matthias Rudin <matthias.rudin@live.com>
2019-03-03 12:33:00 +0100

    Add css flexbox and grid for layout purpose

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="slider">
-        <section>
-         <h1>Section One</h1>
-        </section>
-        <section>
-          <h1>Section Two</h1>
-        </section>
-        <section>
-          <h1>Section Three</h1>
-        </section>
-        <section>
-          <h1>Section Four</h1>
-        </section>
-        <section>
-          <h1>Section Five</h1>
-        </section>
+      <div className="container">
+        <nav>
+          <span>Home</span>
+          <span>Portfolio</span>
+          <span>About</span>
+        </nav>
+        <div className="slider">
+          <section>
+          <h1>Section One</h1>
+          </section>
+          <section>
+            <h1>Section Two</h1>
+          </section>
+          <section>
+            <h1>Section Three</h1>
+          </section>
+        </div>
+        <footer>
+          <span>Copyright 2019 Matt Rudin</span>
+        </footer>
       </div>
     );
   }
